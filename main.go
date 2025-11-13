da549a36b1005676fb6c3a5c50f295d4f50e37a0
YaYan-dev <so66sergo@gmail.com>
2025-11-13 13:25:34 +0800

    add Get/post/patch/delete

--- main.go
+++ main.go
@@ -3,30 +3,80 @@ package main
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var task string = "Task"
+type TaskSt struct {
+	ID     string `json:"id"`
+	Build  string `json:"build"`
+	Result string `json:"result"`
+}
 
-type requestBody struct {
-	Task string `json:"task"`
+type RequestBody struct {
+	Build string `json:"build"`
+}
+
+var task = []TaskSt{}
+
+func buildTask(build string) string {
+	return "Hello, " + build
 }
 
 func getTask(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello "+task)
+	return c.JSON(http.StatusOK, task)
 }
 
 func postTask(c echo.Context) error {
-	var req requestBody
+	var req RequestBody
 
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
 	}
 
-	task = req.Task
+	result := buildTask(req.Build)
 
-	return c.JSON(http.StatusOK, map[string]string{"message": "Task update successfully", "task": task})
+	tas := TaskSt{
+		ID:     uuid.NewString(),
+		Build:  req.Build,
+		Result: result,
+	}
+	task = append(task, tas)
+	return c.JSON(http.StatusCreated, tas)
+}
+
+func patchTask(c echo.Context) error {
+	id := c.Param("id")
+
+	var req RequestBody
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	result := buildTask(req.Build)
+
+	for i, taskItem := range task {
+		if taskItem.ID == id {
+			task[i].Build = req.Build
+			task[i].Result = result
+			return c.JSON(http.StatusOK, task[i])
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task (id) not found"})
+}
+
+func deleteTask(c echo.Context) error {
+	id := c.Param("id")
+
+	for i, taskItem := range task {
+		if taskItem.ID == id {
+			task = append(task[:i], task[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task (id) not found"})
 }
 
 func main() {
@@ -35,8 +85,10 @@ func main() {
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
-	e.GET("/", getTask)
+	e.GET("/task", getTask)
 	e.POST("/task", postTask)
+	e.PATCH("/task/:id", patchTask)
+	e.DELETE("task/:id", deleteTask)
 
 	// запуск сервера на порту 8080
 	e.Logger.Fatal(e.Start(":8080"))
